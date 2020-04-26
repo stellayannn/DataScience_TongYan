93e0dadc40cad4541c2998220089554b4f349dd4
stellayannn <60204267+stellayannn@users.noreply.github.com>
2020-04-26 05:31:05 -0500

    Add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to u use the appstore_games dataset to predict the average user ratings of mobile strategy games.',
+    author='TongYan',
+    license='MIT',
+)
